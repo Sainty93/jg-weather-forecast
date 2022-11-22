c4361ce62df8bfd8cd4734af4bfddca2ed342eaa
Sainty93 <jenandfloyd93@icloud.com>
2022-11-21 18:42:11 -0800

    started javascript

---
+++ assets/script.js
@@ -0,0 +1,15 @@
+var citiesList =$("#cities-List");
+var cities = [];
+var key = "fc8bffadcdca6a94d021c093eac22797";
+
+function FormatDay(date){
+    var date = new Date();
+    console.long(date);
+    var month = date.getMonth()+1;
+    var day = date.getDate();
+
+    var dayOutput =date.getFullyear() + '/' +
+    (month<10 ? '0' : '') + month + '/' +
+    (day<10 ? '0' : '') + day;
+    return dayOutput;
+}
